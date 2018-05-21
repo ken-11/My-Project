dcda3c40c05c02f591a65266ef2a4ed2c2270a59
Ken-lxy <38093104+Ken-lxy@users.noreply.github.com>
2018-05-21 22:06:19 +0800

    Update pipelines.py

--- Fiction/Fiction/pipelines.py
+++ Fiction/Fiction/pipelines.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 # import codecs
 import json
-# import MySQLdb
-# import pymysql
+import MySQLdb
+import pymysql
 # from twisted.enterprise import adbapi
 from scrapy.conf import settings
 
@@ -27,30 +27,29 @@ class FictionPipeline(object):
         self.file.close()
 
 
-# class DBPipeline(object):
+class DBPipeline(object):
 
-#     def process_item(self, item, spider):
-#         host = settings['MYSQL_HOST']
-#         db = settings['MYSQL_DBNAME']
-#         user = settings['MYSQL_USER']
-#         passwd = settings['MYSQL_PASSWD']
-#         charset = 'utf8'
-
-#         con = pymysql.connect(host=host, db=db,
-#                               user=user, passwd=passwd,
-#                               charset=charset, use_unicode=True)
-#         cur = con.cursor()
-#         sql = "insert into Qik(top,url,type,name,update_time,author)" \
-#               " value(%s,%s,%s,%s,%s,%s)"
-#         params = (item['top'], item['url'], item['type_'],
-#                   item['name'], item['update_time'], item['author'])
-#         try:
-#             cur.execute(sql, params)
-#         except Exception as e:
-#             print(e)
-#             con.rollback()
-#         else:
-#             con.commit()
-#         cur.close()
-#         con.close()
-#         return item
+     def process_item(self, item, spider):
+         host = settings['MYSQL_HOST']
+         db = settings['MYSQL_DBNAME']
+         user = settings['MYSQL_USER']
+         passwd = settings['MYSQL_PASSWD']
+         charset = 'utf8'
+         con = pymysql.connect(host=host, db=db,
+                               user=user, passwd=passwd,
+                               charset=charset, use_unicode=True)        
+                               cur = con.cursor()
+         sql = "insert into Qik(top,url,type,name,update_time,author)" \
+              " value(%s,%s,%s,%s,%s,%s)"
+         params = (item['top'], item['url'], item['type_'],
+                 item['name'], item['update_time'], item['author'])
+         try:
+            cur.execute(sql, params)
+         except Exception as e:
+            print(e)
+            con.rollback()
+         else:
+            con.commit()
+         cur.close()
+         con.close()
+         return item
